f2c2f699c95468bbbef7e93874ee4e83dafcd724
josh conduah <jconduah729@yahoo.com>
2024-02-11 00:20:05 -0500

    first version

---
+++ calc-app/src/DigitButton.jsx
@@ -0,0 +1,14 @@
+import { ACTIONS } from "./App"
+
+
+function DigitButton({dispatch, digit}) {
+    return (
+    <button 
+    onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+    {digit}
+    </button>
+    )
+}
+
+export default DigitButton
